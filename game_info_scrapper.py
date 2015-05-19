88d837c70080439e6291bc04d693924895f7acf4
madnoodles <madnoodles@madnoodless-Mac.local>
2015-05-19 00:29:18 -0700

    add game price scrapper and game info scrapper

---
+++ game_info_scrapper.py
@@ -0,0 +1,74 @@
+import urllib2
+from bs4 import BeautifulSoup
+import difflib
+import re
+url_base = "http://www.gamespot.com/new-games/?sort=score&game_filter_type%5Bplatform%5D=19&game_filter_type%5Bgenres%5D=&game_filter_type%5BminRating%5D=&game_filter_type%5BtimeFrame%5D=&game_filter_type%5BstartDate%5D=&game_filter_type%5BendDate%5D=&game_filter_type%5Btheme%5D=&game_filter_type%5Bregion%5D=&game_filter_type%5Bletter%5D=&page="
+page = 1
+starting_url = url_base + str(page) 
+response = urllib2.urlopen(starting_url)
+soup = BeautifulSoup(response)
+domain = "http://www.gamespot.com"
+all_page_num = [int((domain + link.get('href')).replace(url_base,'')) for link in soup.find_all('a',{'class':'btn'})]
+page_num_max = max(all_page_num)
+ 
+all_pages = [url_base+str(i) for i in range(1,page_num_max+1)]
+ 
+ 
+all_game_links = []
+ 
+for page in all_pages:
+	soup = BeautifulSoup(urllib2.urlopen(page))
+	game_links = [(domain + link.get('href')) for link in soup.find_all('a',{'class':'js-event-tracking'})]
+	all_game_links += game_links
+ 
+ 
+def retrive_game_info_by_link(link):
+	response = urllib2.urlopen(link)
+	soup = BeautifulSoup(response)
+	r = soup.find("div", {"id": "object-stats-wrap"})
+ 
+	#date published
+	date_published = r.find('dd', {'class':'pod-objectStats-info__release'}).find('span').text	
+	#system
+	systems = [i.text for i in r.find_all('li', {'class':'system'})]
+	#title
+	name = r.find('a',  {'data-event-tracking':'Tracking|games_overview|GameStats|Title'}).find(text=True).split('\n')[0]
+	#gs score
+	score = float(re.sub("\D", "", r.find('div', {'class':'gs-score__cell'}).text))
+	# UserReviewScore
+	user_review_score = float(r.find('a',  {'data-event-tracking':'Tracking|games_overview|GameStats|UserReviewScore'}).text)
+	# developer
+	pattern = re.compile('Developer+')
+	developer = [i.text for i in r.find_all('a', {'data-event-tracking':pattern})]
+ 
+	# publisher
+	pattern = re.compile('Publisher+')
+	publisher = [i.text for i in r.find_all('a', {'data-event-tracking':pattern})]
+ 
+	# genre
+	pattern = re.compile('Genre+')
+	genre = [i.text for i in r.find_all('a', {'data-event-tracking':pattern})]
+ 
+ 
+	# theme
+	pattern = re.compile('Theme+')
+	theme = [i.text for i in r.find_all('a', {'data-event-tracking':pattern})]
+ 
+	return {
+		"name":name,
+		"systems":systems,
+		"date_published":date_published,
+		"score":score,
+		"user_review_score":user_review_score,
+		"developer":developer,
+		"publisher":publisher,
+		"genre":genre,
+		"theme":theme
+	}
+ 
+items = []
+for i in all_game_links:
+	try:
+		items.append(retrive_game_info_by_link(i))
+	except:
+		continue
---
+++ game_price_scrapper.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+import requests
+import pandas as pd
+import execjs # for executing javascript
+
+game_ids = ['dragonageinquisition', 'tomclancyssplintercellblacklist']
+
+for id in game_ids:
+	print 'Processing: ', id 
+	url = 'http://isthereanydeal.com/ajax/game/price?plain=%s' % id
+
+	response = requests.get(url)
+	cols = ''
+	rows = ''
+	# find js code lines with data
+	for line in response.iter_lines():
+		line = line.strip()
+		if line.startswith('cols'):
+			cols = ':'.join(line.split(':')[1:]).strip(',')
+		elif line.startswith('rows'):
+			rows = ':'.join(line.split(':')[1:]).strip()
+	# eval js to to get data		
+	rows = execjs.eval(rows)
+	cols = execjs.eval(cols)
+	# clean up cols and rows
+	cols = [col.get('label') if col.get('label') else col.get('p').get('role') for col in cols]
+	rows = [[f['v'] for f in row['c']] for row in rows]
+
+	df = pd.DataFrame(rows)
+	df.columns = cols
+
+	df.to_csv('%s.csv' %id)
+	print '-- generated csv'
